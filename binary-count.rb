ddaecac67e5929d57c443b977f7355a3cc96bf70
MD AFTAB ALAM <mdaftab88@gmail.com>
2017-06-15 11:58:46 +0530

    problems

--- binary-count.rb
+++
@@ -1,9 +0,0 @@
-2.4.0 :009 > "MIGHTY".each_char { |chr|
-2.4.0 :010 >     if "GY".include? chr
-2.4.0 :011?>     print chr
-2.4.0 :012?>     else
-2.4.0 :013 >       print "*"
-2.4.0 :014?>     end
-2.4.0 :015?>   }
-**G**Y => "MIGHTY" 
-
